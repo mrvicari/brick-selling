2ceb8edab826314d25dd328014bbfd3f11c8b733
Miguel Restrepo Vicari <miguelrestrepovicari@Miguels-MacBook-Pro-2.local>
2018-06-03 20:39:25 +0100

    Creat test for 'Create order' at service layer

--- src/test/java/com/mrvicari/brickselling/service/ServiceTest.java
+++ src/test/java/com/mrvicari/brickselling/service/ServiceTest.java
@@ -1,5 +1,43 @@
 package com.mrvicari.brickselling.service;
 
+import com.mrvicari.brickselling.model.BrickOrder;
+import com.mrvicari.brickselling.repository.OrderRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceTest
 {
+    private static BrickOrder TEST_ORDER;
+    private static final Integer NUM_OF_BRICKS = 5;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    private OrderService orderService;
+
+    @Before
+    public void setup()
+    {
+        orderService = new OrderService(orderRepository);
+
+        TEST_ORDER = new BrickOrder(1, 5);
+    }
+
+    @Test
+    public void createOrder_searchDb_returnOrder()
+    {
+        when(orderRepository.save(any())).thenReturn(TEST_ORDER);
+
+        BrickOrder newOrder = orderService.createOrder(new BrickOrder(1, NUM_OF_BRICKS));
+
+        assertThat(newOrder.getNumOfBricks()).isEqualTo(NUM_OF_BRICKS);
+    }
 }
