fb1f8980fdd9f458c6e1d987f364b0bf1a04f1ae
Miguel Restrepo Vicari <miguelrestrepovicari@Miguels-MacBook-Pro-2.local>
2018-06-03 22:14:04 +0100

    Redo repository tests due to change in entity

--- src/test/java/com/mrvicari/brickselling/repository/RepositoryTest.java
+++ src/test/java/com/mrvicari/brickselling/repository/RepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
@@ -30,10 +32,29 @@ public class RepositoryTest
     @Test
     public void saveOrder_findByReference()
     {
-        BrickOrder newOrder = testEntityManager.persistAndFlush(new BrickOrder(1, 5));
+        BrickOrder newOrder = new BrickOrder();
+        newOrder.setNumOfBricks(5);
+
+        testEntityManager.persistAndFlush(newOrder);
         BrickOrder foundOrder = orderRepository.findByReference(1);
 
         assertThat(foundOrder.getReference()).isEqualTo(newOrder.getReference());
         assertThat(foundOrder.getNumOfBricks()).isEqualTo(newOrder.getNumOfBricks());
+    }
+
+    @Test
+    public void getAllOrders_returnList()
+    {
+        BrickOrder order1 = new BrickOrder();
+        BrickOrder order2 = new BrickOrder();
+        order1.setNumOfBricks(5);
+        order2.setNumOfBricks(10);
+
+        testEntityManager.persistAndFlush(order1);
+        testEntityManager.persistAndFlush(order2);
+
+        List<BrickOrder> allOrders = orderRepository.findAll();
+
+        assertThat(allOrders.size()).isEqualTo(2);
     }
 }
