b3d4400d8934fef6f2005c98236beabf32602e8a
Miguel Restrepo Vicari <miguelrestrepovicari@Miguels-MacBook-Pro-2.local>
2018-06-03 19:52:46 +0100

    Initial commit

---
+++ src/main/java/com/mrvicari/brickselling/BrickSellingApplication.java
@@ -0,0 +1,12 @@
+package com.mrvicari.brickselling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickSellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickSellingApplication.class, args);
+	}
+}
